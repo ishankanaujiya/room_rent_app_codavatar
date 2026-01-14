089981be13fbe577b455cebec11f9be7252af644
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2026-01-14 12:29:36 +0545

    Interactive UI For Login Created

--- lib/screen/loginScreen.dart
+++ lib/screen/loginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:room_rent_app/screen/registrationScreen.dart';
 import 'package:room_rent_app/util/customColor.dart';
 import 'package:room_rent_app/widget/buttonWidget.dart';
 import 'package:room_rent_app/widget/textFieldWidget.dart';
@@ -10,84 +11,136 @@ class LoginInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 250, 248, 255),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(top: 10.0, bottom: 20.0),
-            width: double.infinity,
-
-            decoration: BoxDecoration(
-              color: Color(0xFFB794F4),
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(200)),
-              boxShadow: [
-                BoxShadow(
-                 color: Color(0xFFB794F4).withOpacity(0.7),
-                 blurRadius: 10,
-                 spreadRadius: 6,
-                 
-                )
-              ]
-            ),
-          
-            child: Container(
-              width: double.infinity,
-              height: 300,
-              // color: Colors.cyan,
-              child: Image(image: AssetImage("logInScreenPersonPicture.png"),fit: BoxFit.contain,),
-            ),
-          ),
-
-          SizedBox(
-          
-            height: 40.0,
-          ),
-
-
-          Text("Welcome To Room Khoj", style: TextStyle(
-            fontSize: 13,
-            fontWeight: FontWeight.bold,
-            color: Colors.grey.withOpacity(0.5),
-            letterSpacing: 2.0,
-          ),),
-
-          SizedBox(
-          
-            height: 10.0,
-          ),
-
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
             children: <Widget>[
-              Text("Sign", style: TextStyle(
-                fontSize: 30,
+              Container(
+                padding: EdgeInsets.only(top: 10.0, bottom: 20.0),
+                width: double.infinity,
+          
+                decoration: BoxDecoration(
+                  color: Color(0xFFB794F4),
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(200)),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Color(0xFFB794F4).withOpacity(0.7),
+                     blurRadius: 10,
+                     spreadRadius: 6,
+                     
+                    )
+                  ]
+                ),
+              
+                child: Container(
+                  width: double.infinity,
+                  height: 300,
+                  // color: Colors.cyan,
+                  child: Image(image: AssetImage("logInScreenPersonPicture.png"),fit: BoxFit.contain,),
+                ),
+              ),
+          
+              SizedBox(
+              
+                height: 40.0,
+              ),
+          
+          
+              Text("Welcome To Room Khoj", style: TextStyle(
+                fontSize: 13,
                 fontWeight: FontWeight.bold,
-                color: CustomColor.primaryTextColor.withOpacity(0.9),
+                color: Colors.grey.withOpacity(0.5),
+                letterSpacing: 2.0,
               ),),
+          
+              SizedBox(
+              
+                height: 10.0,
+              ),
+          
+          
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Sign", style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: CustomColor.primaryTextColor.withOpacity(0.9),
+                  ),),
+          
+                  Text("In", style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF9547BF),
+                  ),),
+                ],
+              ),
+          
+              SizedBox(
+              
+                height: 20.0,
+              ),
+              SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                  width: double.infinity,
+                  height: 160,
+                  // color: Colors.cyan,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      TextFieldWidget(passwordVisibility: false, emailTextField: true,),
+                      TextFieldWidget(passwordVisibility: true, emailTextField: false,),
+                    ],
+                  )),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+            
+              ButtonWidget(),
+          
+              SizedBox(
+                    height: 40.0,
+                  ),
+                  
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Don't have an account?", style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                    color: CustomColor.primaryTextColor.withOpacity(0.9),
+                  ),),
 
-              Text("In", style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF9547BF),
-              ),),
+                
+                SizedBox(
+                  width: 10.0,
+                ),
+                  
+          
+                  InkWell(
+                    onTap: ()
+                    {
+                       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => RegistrationScreen()
+                        ));
+                    },
+                    child: Text("Sign Up", style: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF9547BF),
+                    ),),
+                  ),
+                  
+                  
+          
+                ],
+              )
+          
             ],
           ),
-
-          SizedBox(
-          
-            height: 20.0,
-          ),
-          TextFieldWidget(passwordVisibility: false, emailTextField: true,),
-          SizedBox(
-            height: 20.0,
-          ),
-          TextFieldWidget(passwordVisibility: true, emailTextField: false,),
-           SizedBox(
-            height: 30.0,
-          ),
-          ButtonWidget(),
-
-        ],
+        ),
       ),
     );
   }
--- lib/widget/buttonWidget.dart
+++ lib/widget/buttonWidget.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:room_rent_app/screen/homeScreen.dart';
+import 'package:room_rent_app/screen/loginScreen.dart';
 import 'package:room_rent_app/util/customColor.dart';
 
 class ButtonWidget extends StatefulWidget {
-  const ButtonWidget({super.key});
+  final Widget? navigatorWidget;
+  const ButtonWidget({super.key, this.navigatorWidget});
 
   @override
   State<ButtonWidget> createState() => _ButtonWidgetState();
@@ -11,20 +14,28 @@ class ButtonWidget extends StatefulWidget {
 class _ButtonWidgetState extends State<ButtonWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 50,
-      decoration: BoxDecoration(
-        color: CustomColor.borderDividerColor,
-        borderRadius: BorderRadius.circular(10),
+    return InkWell(
+      onTap: ()
+      {
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen()
+        ));
 
-      ),
-      child: Center(
-        child: Text("Sign In", style: TextStyle(
-          color: Colors.white,
-          fontSize: 14,
-          fontWeight: FontWeight.w400,
-        ),),
+      },
+      child: Container(
+        width: 200,
+        height: 50,
+        decoration: BoxDecoration(
+          color: CustomColor.borderDividerColor,
+          borderRadius: BorderRadius.circular(10),
+      
+        ),
+        child: Center(
+          child: Text("Sign In", style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          ),),
+        ),
       ),
     );
   }
