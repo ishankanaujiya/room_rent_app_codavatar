2a83bde168476008acd6ca123587907d53719688
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2026-01-19 08:21:27 +0545

    Multiple Selected Pictures Shown In UI

--- lib/screen/selectMultiplePicture.dart
+++ lib/screen/selectMultiplePicture.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
 
 class SelectMultiplePicture extends StatefulWidget {
@@ -10,28 +13,78 @@ class SelectMultiplePicture extends StatefulWidget {
 
 class _SelectMultiplePictureState extends State<SelectMultiplePicture> {
 
+      ImagePicker pickPicture = new ImagePicker();
+
+      List<XFile> selectedPicture = [];
+
+      List<File> convertedPicture = [];
 
   selectMultiplePicture() async
   {
-      ImagePicker pickPicture = new ImagePicker();
 
-      List<XFile>? selectedPicture = await pickPicture.pickMultiImage();
+      selectedPicture = await pickPicture.pickMultiImage(
+        imageQuality: 80,
+        // maxWidth: 1024,
+        // maxHeight: 1024,
+      );
 
       for(int i = 0; i<selectedPicture.length; i++)
       {
         print("The Selected Pictures are: ${selectedPicture[i].path}");
+      
       }
+      setState(() {
+        
+      });
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(onPressed: ()
-        {
-          selectMultiplePicture();
-        },
-         child: Text("Select Picture"),
-         ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity.w,
+            height: 200.h,
+            color: Colors.cyan,
+            child: Center(
+              child: ElevatedButton(onPressed: ()
+              {
+                selectedPicture.clear();
+                setState(() {
+                  
+                });
+                selectMultiplePicture();
+              
+              },
+               child: Text("Select Picture"),
+               ),
+            ),
+          ),
+
+          Expanded(
+            child: Container(
+              width: double.infinity.w,
+              height: 400.h,
+              color: Colors.white,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: selectedPicture.length,
+                itemBuilder: (context, index)
+                {
+                  return Container(
+                    margin: EdgeInsets.only(right: 20.0),
+                    width: 200.w,
+                    height: 200.h,
+                    // color: Colors.black,
+                    child: Image.file(File(selectedPicture[index].path), fit: BoxFit.contain,),
+                  );
+                
+                }),
+            ),
+          )
+
+          
+        ],
       ),
     );
   }
