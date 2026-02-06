bcfa083e516d6d4bdc4b9e67a04402db4c1d0c67
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2026-02-06 08:14:49 +0545

    End Points For ProfileDetail Modification Along With Profile Picture Modification Created

---
+++ lib/screen/editProfileScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EditProfileSreen extends StatefulWidget {
+  const EditProfileSreen({super.key});
+
+  @override
+  State<EditProfileSreen> createState() => _EditProfileSreenState();
+}
+
+class _EditProfileSreenState extends State<EditProfileSreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
--- lib/service/firebaseService.dart
+++ lib/service/firebaseService.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseService
 {
@@ -25,5 +26,25 @@ class FirebaseService
   Future<Stream<QuerySnapshot>> getRoomDetail() async
   {
     return await firebaseFirestoreForRoomDetail.snapshots();
+  }
+
+  updateUsersDetail(Map<String, dynamic> updatedDetail, String registeredEmail) async
+  {
+    try
+    {
+      QuerySnapshot userDetail = await firebaseFirestore.where("Email", isEqualTo: registeredEmail).get();
+      DocumentSnapshot documentSnapshot = userDetail.docs.first;
+      String docId = documentSnapshot.id;
+      
+      return await firebaseFirestore.doc(docId).update({
+        "Full Name" : updatedDetail['FullName'],
+        "Phone Number" : updatedDetail['PhoneNumber'],
+        "SecureUrl" : updatedDetail['secureUrl'],
+      });
+    }
+    catch(e)
+    {
+      print(e.toString());
+    }
   }
 }
