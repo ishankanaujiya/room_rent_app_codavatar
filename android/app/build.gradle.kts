d4986a7a47a1d008fb2d198062fd1f721948c0ed
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2026-03-04 15:43:15 +0545

    Keystore Modified

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,9 @@ android {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storeFile = keystoreProperties["storeFile"]?.let {
+                rootProject.file(it as String)
+            }
             storePassword = keystoreProperties["storePassword"] as String
         }
     }
